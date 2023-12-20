57a90a84dc42ec3ad4eeac850b80af25af4f6989
khanhvu0711 <48041727+khanhvu0711@users.noreply.github.com>
2023-12-20 15:52:58 +0700

    Update solonicehash.sh

--- solonicehash.sh
+++ solonicehash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ccminernicehash -a verus -o stratum+tcp://sg.vipor.net:5045 -u RUQRvxh9hFdJyLVLBcg5jQKYbkVvdaQYFd.a750. -p x -t 6
+./ccminernicehash -a verus -o stratum+tcp://sg.vipor.net:5045 -u RUQRvxh9hFdJyLVLBcg5jQKYbkVvdaQYFd.miphavipor -p x -t 6
