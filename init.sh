42fb63e78ef001eb16d15888e3b919def2155bec
khanhvu0711 <48041727+khanhvu0711@users.noreply.github.com>
2023-12-20 15:14:13 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ chmod +x ccminer
 chmod +x config.json
 chmod +x a750mining.sh
 chmod +x ccminerARM
-apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential vim -y
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential libomp5 vim -y
