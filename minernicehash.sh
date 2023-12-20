d041dfca96d17c879a1fc635801e24f926a65007
khanhvu0711 <48041727+khanhvu0711@users.noreply.github.com>
2023-12-20 17:48:49 +0700

    Update minernicehash.sh

--- minernicehash.sh
+++ minernicehash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RUQRvxh9hFdJyLVLBcg5jQKYbkVvdaQYFd.node -p x -t 6
+./ccminernicehash -a verus -o stratum+tcp://sg.vipor.net:5040 -u RUQRvxh9hFdJyLVLBcg5jQKYbkVvdaQYFd.node -p x -t 6
