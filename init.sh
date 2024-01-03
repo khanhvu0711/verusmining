a1764f35c7121a1b78ae4e6d0458599799f2d7dc
khanhvu0711 <48041727+khanhvu0711@users.noreply.github.com>
2024-01-03 16:38:36 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,4 @@ chmod +x solonicehash.sh
 chmod +x poolnicehash.sh
 chmod +x config.json
 
-echo 'cd verusmining\n./poolnicehash.sh' >> /etc/bash.bashrc
 apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential libomp5 vim -y
