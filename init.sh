72bb046531c027a8b4066e47208b7de47aff106f
khanhvu0711 <48041727+khanhvu0711@users.noreply.github.com>
2024-01-03 16:26:28 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,8 +2,7 @@
 chmod +x ccminernicehash
 chmod +x ccminer
 chmod +x solonicehash.sh
-chmod +x minernicehash.sh
+chmod +x poolnicehash.sh
 chmod +x config.json
-chmod +x a750mining.sh
-chmod +x ccminerARM
+
 apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential libomp5 vim -y
